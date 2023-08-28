fc5aec9f9f46ac34c03c75a6bc6d0f358a4cdf13
Aman <amankr0741@gmail.com>
2023-08-28 22:02:12 +0530

    .env file created

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 
 const express = require('express');
 const app = express()
-
+const path = require("path");
 
 // Your code goes here
+const schema = require("./models/subscribers"); // Import the subscriber model
+// const { error } = require("console"); // Import the 'error' object from the console module
+app.get("/", (req, res) => {
+    res.send("hello World!"); // Serve the index.html file as the home page
+  });
 
+app.get("/subscribers", async (req, res, next) => {
+    try {
+      let subscribers = await schema.find(); // Retrieve all subscribers from the schema/model
+      res.status(200).json(subscribers); // Send the subscribers as a JSON response with a status of 200 (OK)
+    } catch (err) {
+      res.status(400); // Set the response status to 400 (Bad Request)
+      next(err); // Pass the error to the error handling middleware
+    }
+  });
 
-
+  module.exports = app;
 
 
 
--- src/createDatabase.js
+++ src/createDatabase.js
@@ -1,18 +1,25 @@
 const mongoose = require('mongoose')
 const subscriberModel = require('./models/subscribers')
 const data = require('./data')
-
+require('dotenv').config();
 // Connect to DATABASE
-const DATABASE_URL = "mongodb://localhost/subscribers";
+const DATABASE_URL = process.env.URL;
 mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
 db.once('open', () => console.log('Database created...'))
-
 const refreshAll = async () => {
-    await subscriberModel.deleteMany({})
-    // console.log(connection)
-    await subscriberModel.insertMany(data)
-    await mongoose.disconnect();
-}
-refreshAll()
+    try {
+      await subscriberModel.deleteMany({});
+      const insertResult = await subscriberModel.insertMany(data);
+      console.log('Data inserted successfully:', insertResult);
+    } catch (error) {
+      console.error('Error inserting data:', error);
+    } finally {
+      await mongoose.disconnect();
+    }
+  };
+  
+  refreshAll();
+  
+  
