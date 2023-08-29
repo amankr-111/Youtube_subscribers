4a12cccffba536a0f37c8f465e7c92b71c59a1ef
Aman <amankr0741@gmail.com>
2023-08-29 17:07:04 +0530

    Routes created

--- src/app.js
+++ src/app.js
@@ -19,24 +19,42 @@ app.get("/subscribers", async (req, res, next) => {
       next(err); // Pass the error to the error handling middleware
     }
   });
-
+  app.get("/subscribers/names", async (req, res, next) => {
+    try {
+      let subscribers = await schema.find(
+        {},
+        { name: 1, subscribedChannel: 1, _id: 0 }
+      ); // Retrieve subscribers with only the name and subscribedChannel fields from the schema/model
+      res.status(200).json(subscribers); // Send the subscribers as a JSON response with a status of 200 (OK)
+    } catch (err) {
+      res.status(400); // Set the response status to 400 (Bad Request)
+      next(err); // Pass the error to the error handling middleware
+    }
+  });
+  
+  // THIS ROUTE PROVIDES THE DETAILS OF SUBSCRIBER WITH THE GIVEN ID.
+  app.get("/subscribers/:id", async (req, res) => {
+    try {
+      const id = req.params.id; // Extract the ID parameter from the request URL
+      if (!id) {
+        res.status(400).json({ message: "No ID provided" }); // Send a JSON response with a status of 400 (Bad Request)
+        return;
+      }
+      const subscriber = await schema.findById(id); // Find a subscriber with the given ID in the schema/model
+      if (!subscriber) {
+        res.status(404).json({ message: "Subscriber not found" }); // Send a JSON response with a status of 404 (Not Found)
+        return;
+      }
+      res.send(subscriber); // Send the subscriber details as the response
+    } catch (error) {
+      res.status(400).json({ message: error.message }); // Send a JSON response with a status of 400 (Bad Request) and the error message
+    }
+  });
+  
+  // HANDLES ALL THE UNWANTED REQUESTS.
+  app.use((req, res) => {
+    res.status(404).json({ message: "Error - Route not found" }); // Send a JSON response with a status of 404 (Not Found)
+  });
+  
   module.exports = app;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
